e470347eb4ab1b342d3f698a1f96d6354c7e5686
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-07-23 09:14:07 -0700

    Add unfold.
    Reviewed by @tolmasky.

--- babel-map-accum/babel-map-accum.js
+++ babel-map-accum/babel-map-accum.js
@@ -3,52 +3,81 @@ const t = require("@babel/types");
 const mapAccumArray = require("@climb/map-accum");
 
 
+console.log("hi!");
 module.exports = function babelMapAccum({ concat, identity }, fOrDefinitions)
 {
     const f = typeof fOrDefinitions === "object" ?
         fromDefinitions(fOrDefinitions) : fOrDefinitions;
-
-    return function babelMapAccum(node)
+    const keyed = pairs => mapAccumArray(
+        (accumIn, [node, key]) =>
+            (([accumOut, mapped]) => [concat(accumIn, accumOut, key), mapped])
+            (mapAccum(node)),
+        identity,
+        pairs);
+    const mapAccum = Object.assign(function mapAccum(node)
     {
         if (node === null || node === void(0))
             return [identity, node];
 
         if (isArray(node))
         {
-            const [accumOut, mapped] = mapAccumArray(
-                (accumIn, node) =>
-                    (([accumOut, mapped]) => [concat(accumIn, accumOut), mapped])
-                    (babelMapAccum(node)), identity, node);
-            const modified = mapped.some((mapped, index) => mapped !== node[index]);
+            const [accumOut, mapped] =
+                keyed(node.map((node, index) => [node, index]));
+            const modified = mapped.some(
+                (mapped, index) => mapped !== node[index]);
 
             return [accumOut, modified ? mapped : node];
         }
 
-        return f(babelMapAccum, node);
-    }
+        return f(mapAccum, node);
+    }, { keyed, identity, concat, fallback: f.fallback });
+
+    return mapAccum;
 }
 
-module.exports.fromDefinitions = function fromDefinitions(definitions, fallback)
+module.exports.fromDefinitions = function fromDefinitions(definitions, fallback = defaultFallback)
 {
-    return function f(babelMapAccum, node)
+    return Object.assign(function f(babelMapAccum, node)
     {
-        if (definitions[node.type])
-            return definitions[node.type](babelMapAccum, node);
+        const closest = definitions[node.type] ?
+            node.type : t.ALIAS_KEYS[node.type].find(key => definitions[key]);
+        const definition = definitions[closest] || fallback;
 
-        if (fallback)
-            return fallback(babelMapAccum, node);
-
-        const fields = t.VISITOR_KEYS[node.type];
-        const children = fields.map(field => node[field]);
-        const [accumOut, mapped] = babelMapAccum(children);
-        const modified = children
-            .map((child, index) => [child, index, fields[index]])
-            .filter(([child, index]) => child !== mapped[index]);
-        const newNode = modified.length === 0 ?
-            node :
-            modified.reduce((accum, [, index, field]) =>
-                (accum[field] = mapped[index], accum), { ...node });
-
-        return [accumOut, newNode];
-    }
+        return definition(babelMapAccum, node);
+    }, { fallback });
 }
+
+function defaultFallback(mapAccum, node)
+{
+    const fields = toVisitorKeys(node);
+    const tuples = fields.map((field, index) => [node[field], field, index]);
+    const [accumOut, mapped] = mapAccum.keyed(tuples);
+    const modified = tuples
+        .filter(([child], index) => child !== mapped[index]);
+    const newNode = modified.length === 0 ?
+        node :
+        modified.reduce((accum, [, field, index]) =>
+            (accum[field] = mapped[index], accum), { ...node });
+
+    return [accumOut, newNode];
+}
+
+const toVisitorKeys = (function ()
+{
+    const fields = t.VISITOR_KEYS;
+    const withold = target => field => field !== target;
+    const fieldsWitholdingComputed = 
+    {
+        MemberExpression: fields.MemberExpression.filter(withold("property")),
+        ObjectProperty: fields.ObjectProperty.filter(withold("key"))
+    };
+
+    return function toVisitorKeys({ type, computed })
+    {
+        return  type !== "MemberExpression" &&
+                type !== "ObjectProperty" ||
+                computed === true ?
+                fields[type] :
+                fieldsWitholdingComputed[type];
+    }
+})();
--- map-accum/map-accum.js
+++ map-accum/map-accum.js
@@ -1,5 +1,25 @@
 module.exports = function mapAccum(f, accumulated, list)
 {
+    const implementation = Array.isArray(list) ?
+        mapAccumArray : mapAccumIterable;
+
+    return implementation(f, accumulated, list);
+}
+
+function mapAccumIterable(f, accumulated, iterable)
+{
+    var index = 0;
+    var result = [];
+
+    for (const item of iterable)
+        [accumulated, result[index++]] =
+            f(accumulated, item);
+
+    return [accumulated, result];
+}
+
+function mapAccumArray(f, accumulated, list)
+{
     const count = list.length;
     const result = [];
 
