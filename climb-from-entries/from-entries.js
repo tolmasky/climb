50d320ab2059a568ed28f5837d2f72ab7dd10d2e
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2019-07-28 16:46:54 -0700

    Fix @climb/from-entries for arrays.
    Reviewed by @tolmasky.

--- climb-from-entries/from-entries.js
+++ climb-from-entries/from-entries.js
@@ -3,8 +3,10 @@ const { isArray, from: ArrayFrom } = Array;
 
 module.exports = Object.fromEntries || function fromEntries(entries)
 {
-    const array = isArray(entries) || ArrayFrom(entries, entry => entry);
-    
+    const array = isArray(entries) ?
+        entries :
+        ArrayFrom(entries, entry => entry);
+
     return array.reduce(
         (object, [key, value]) => (object[key] = value, object),
         { });
